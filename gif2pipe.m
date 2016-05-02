1cd69b0d6c91ed852973385b83ac8bf61bc04aa8
p2admin <renyi.tsinghua@gmail.com>
2016-05-02 16:21:46 -0700

    working on loading data..

--- gif2pipe.m
+++ gif2pipe.m
@@ -1,13 +1,24 @@
-function [pipe]=gif2pipe(gif)
-% eg. gif=imread('dent1.gif','frames','all');
+address = './pipe_as_gif/';
+damage_type = {'dent','slit','impi','squiz','nd'};
+label = [1,2,3,4,5];
+damage_sample_size = 100;
+L = 200;
+resize = 0.5;
+H = 640*resize;
+W = 480*resize;
 
-[a,b,~,d]=size(gif);
-
-pipe=zeros(a,b,d);
-
-for i=1:d
-    im=gif(:,:,:,i);
-    pipe(:,:,i)=im2bw(im);
-end
-
+for t = 1:numel(label)
+    X = zeros(L*damage_sample_size, H*W);
+    for i = 1:damage_sample_size+1
+        fprintf('%d',i);
+        file = [address,damage_type{t},num2str(i),'.gif'];
+        gif=imread(file,'frames','all');
+        for ii= 1:L
+            im=gif(:,:,:,ii);
+            imm = imresize(im2bw(im),resize);
+            X((i-1)*L + ii,:)=imm(:)';
+        end
+    end
+    save_file = [address,damage_type{t},'.mat'];
+    save(save_file,'X','-v7.3');
 end
--- main.py
+++ main.py
@@ -3,6 +3,27 @@ import matplotlib.pyplot as plt
 from sklearn import svm, datasets
 from PIL import Image
 
-im = Image.open('image.gif')
-rgb_im = im.convert('RGB')
-r, g, b = rgb_im.getpixel((1, 1))
+address = './pipe_as_gif/'
+damage_type = ['dent','slit','impi','squiz','nd']
+label = [1,2,3,4,5]
+damage_sample_size = 100
+H = 640
+W = 480
+L = 200
+
+X = np.empty((0, H*W))
+y = np.empty((0))
+for tid, t in enumerate(damage_type):
+    for i in range(1,damage_sample_size+1):
+        file = address + t + str(i) + '.gif'
+        im = Image.open(file).convert('L')
+        im = np.asarray(im.getdata(), dtype=np.uint8).reshape((im.size[1],im.size[0]))
+        w=Image.fromarray(im,mode='L')
+        plt.imshow(w)
+
+        plt.show()
+        # for l in range(L):
+        #     X = X.concat(im)
+        #     y = y.concat(label[tid]*np.ones(L))
+
+
